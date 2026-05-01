2cdca040e5b6866729c0e3093c0d67a56e8c3f15
Asikur Rahaman <mdasikur5893@gmail.com>
2026-05-01 23:18:06 +0600

    response interceptor

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,17 +1,23 @@
 import axios from "axios"
 import useAuth from "./useAuth"
+import { useEffect } from "react";
 const instanceSecure = axios.create({
     baseURL: "http://localhost:3000"
 })
 const useAxiosSecure = () => {
     //!import user form useAuth;
     const { user } = useAuth();
-    //?Add a request interceptor;
-    instanceSecure.interceptors.request.use((config) => {
-        //? headers:authorization set;
-        config.headers.authorization = `Bearer ${user.accessToken}`
-        return config;
-    })
+    useEffect(() => {
+        //?Add a request interceptor;
+        const requestInterceptor = instanceSecure.interceptors.request.use((config) => {
+            //? headers:authorization set;
+            config.headers.authorization = `Bearer ${user.accessToken}`
+            return config;
+        })
+        return () => {
+            instanceSecure.interceptors.request.eject(requestInterceptor)
+        }
+    }, [user])
     return instanceSecure
 }
 export default useAxiosSecure
